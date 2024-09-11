e301946dd27d49de58f3486a70df0a903bb15ce0
Michal Arbet <michal.arbet@ultimum.io>
2024-09-11 19:20:54 +0200

    Backup from active mariadb server
    Proxysql reports its version to MySQL clients,
    and that's why the backup fails when ProxySQL used.
    
    This patch modifies the script that handles backups, so that
    instead of backing up through the load balancer, it just
    connects through the load balancer, retrieves the address,
    and then starts backing up directly from that server.
    
    Closes-Bug: #2080818
    Change-Id: Ibf7e40e8b059d733e114963022df06180249c650

--- docker/mariadb/mariadb-server/backup.sh
+++ docker/mariadb/mariadb-server/backup.sh
@@ -3,12 +3,18 @@
 set -eu
 set -o pipefail
 
+BACKUP_DIR=/backup/
+DEFAULT_MY_CNF="/etc/mysql/my.cnf"
+ACTIVE_MY_CNF="/etc/mysql/my_active.cnf"
+
+cd $BACKUP_DIR
+
 # Execute a full backup
 backup_full() {
     echo "Taking a full backup"
     LAST_FULL_DATE=$(date +%d-%m-%Y)
     mariabackup \
-        --defaults-file=/etc/mysql/my.cnf \
+        --defaults-file=$ACTIVE_MY_CNF \
         --backup \
         --stream=xbstream \
         --parallel=4 \
@@ -27,7 +33,7 @@ backup_incremental() {
         LAST_FULL_DATE=$(date +%d-%m-%Y)
     fi
     mariabackup \
-        --defaults-file=/etc/mysql/my.cnf \
+        --defaults-file=$ACTIVE_MY_CNF \
         --backup \
         --stream=xbstream \
         --parallel=4 \
@@ -36,10 +42,20 @@ backup_incremental() {
         $BACKUP_DIR/incremental-$(date +%H)-mysqlbackup-$(date +%d-%m-%Y-%s).qp.xbc.xbs.gz
 }
 
-BACKUP_DIR=/backup/
-cd $BACKUP_DIR
+get_and_set_active_server() {
+    HOST="$(grep '^host' $DEFAULT_MY_CNF | awk -F '=' '{print $2}' | xargs)"
+    USER="$(grep '^user' $DEFAULT_MY_CNF | awk -F '=' '{print $2}' | xargs)"
+    PASS="$(grep '^password' $DEFAULT_MY_CNF | awk -F '=' '{print $2}' | xargs)"
+
+    SELECT='SELECT HOST FROM information_schema.PROCESSLIST WHERE ID = CONNECTION_ID();'
+    ACTIVE_HOST=$(getent hosts $(mysql -h $HOST -u ${USER} -p${PASS} -s -N -e "$SELECT" | awk -F ':' '{print $1}') | awk '{print $1}')
+    cp $DEFAULT_MY_CNF $ACTIVE_MY_CNF
+    sed -i "s/$HOST/$ACTIVE_HOST/g" $ACTIVE_MY_CNF
+}
+
 
 if [ -n $BACKUP_TYPE ]; then
+    get_and_set_active_server
     case $BACKUP_TYPE in
         "full")
         backup_full
