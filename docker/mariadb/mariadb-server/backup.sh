eef8c85fa1f3d7bc624d5da18cf2ed80023e9606
Matthew N Heler <matthew.heler@hotmail.com>
2024-11-28 20:01:54 -0600

    Revert "Backup from active mariadb server"
    This reverts commit efecde0336c27de63d283fbd5f542a09ebca4adb.

--- docker/mariadb/mariadb-server/backup.sh
+++ docker/mariadb/mariadb-server/backup.sh
@@ -3,18 +3,12 @@
 set -eu
 set -o pipefail
 
-BACKUP_DIR=/backup/
-DEFAULT_MY_CNF="/etc/mysql/my.cnf"
-ACTIVE_MY_CNF="/etc/mysql/my_active.cnf"
-
-cd $BACKUP_DIR
-
 # Execute a full backup
 backup_full() {
     echo "Taking a full backup"
     LAST_FULL_DATE=$(date +%d-%m-%Y)
     mariabackup \
-        --defaults-file=$ACTIVE_MY_CNF \
+        --defaults-file=/etc/mysql/my.cnf \
         --backup \
         --stream=xbstream \
         --parallel=4 \
@@ -33,7 +27,7 @@ backup_incremental() {
         LAST_FULL_DATE=$(date +%d-%m-%Y)
     fi
     mariabackup \
-        --defaults-file=$ACTIVE_MY_CNF \
+        --defaults-file=/etc/mysql/my.cnf \
         --backup \
         --stream=xbstream \
         --parallel=4 \
@@ -42,20 +36,10 @@ backup_incremental() {
         $BACKUP_DIR/incremental-$(date +%H)-mysqlbackup-$(date +%d-%m-%Y-%s).qp.xbc.xbs.gz
 }
 
-get_and_set_active_server() {
-    HOST="$(grep '^host' $DEFAULT_MY_CNF | awk -F '=' '{print $2}' | xargs)"
-    USER="$(grep '^user' $DEFAULT_MY_CNF | awk -F '=' '{print $2}' | xargs)"
-    PASS="$(grep '^password' $DEFAULT_MY_CNF | awk -F '=' '{print $2}' | xargs)"
-
-    SELECT='SELECT HOST FROM information_schema.PROCESSLIST WHERE ID = CONNECTION_ID();'
-    ACTIVE_HOST=$(getent hosts $(mysql -h $HOST -u ${USER} -p${PASS} -s -N -e "$SELECT" | awk -F ':' '{print $1}') | awk '{print $1}')
-    cp $DEFAULT_MY_CNF $ACTIVE_MY_CNF
-    sed -i "s/$HOST/$ACTIVE_HOST/g" $ACTIVE_MY_CNF
-}
-
+BACKUP_DIR=/backup/
+cd $BACKUP_DIR
 
 if [ -n $BACKUP_TYPE ]; then
-    get_and_set_active_server
     case $BACKUP_TYPE in
         "full")
         backup_full
